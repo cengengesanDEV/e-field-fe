3c496f4a1bec6d9c91520dd598dc71e9d6a88bbf
mindkeeper <fcb.nyak@gmail.com>
2023-11-23 23:26:48 +0700

    add logout modal

--- src/components/parents/Navbar.js
+++ src/components/parents/Navbar.js
@@ -1,123 +1,159 @@
-import React from "react";
-import css from "./css/Navbar.module.css";
-import { Link } from "react-router-dom";
-import Logos from "../../assets/JakartaLogo1.png";
-import Pattern from "../../assets/patternav.jpg";
-import { useSelector } from "react-redux";
-import { Button } from "antd";
+import React, { useCallback, useState } from 'react';
+import css from './css/Navbar.module.css';
+import { Link } from 'react-router-dom';
+import Logos from '../../assets/JakartaLogo1.png';
+import Pattern from '../../assets/patternav.jpg';
+import { useSelector } from 'react-redux';
+import { Button, Modal } from 'antd';
 
 function Navbar() {
   const profile = useSelector((state) => state.auth.profile);
   const token = useSelector((state) => state.auth.token);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleLogout = useCallback(async () => setIsModalOpen(false), []);
   return (
     <>
       <div className={css.headNavbar}>
-        <img src={Logos} alt="logoJakarta" width={"50px"} height={"50px"} style={{ backgroundSize: "cover" }} />
+        <img
+          src={Logos}
+          alt='logoJakarta'
+          width={'50px'}
+          height={'50px'}
+          style={{ backgroundSize: 'cover' }}
+        />
         <p className={css.title}>Soccer Fields DKI Jakarta</p>
-        <img src={Logos} alt="logoJakarta" width={"50px"} height={"50px"} style={{ backgroundSize: "cover" }} />
+        <img
+          src={Logos}
+          alt='logoJakarta'
+          width={'50px'}
+          height={'50px'}
+          style={{ backgroundSize: 'cover' }}
+        />
       </div>
       <div className={css.headListNavbar}>
         <div className={css.bodyList}>
-          {profile.role == "customer" ? (
+          {profile.role == 'customer' ? (
             <>
-              <Link to="/dashboard" className={css.list}>
+              <Link to='/dashboard' className={css.list}>
                 Dashboard
               </Link>
-              <p to="#" className={css.Pembatas}>
+              <p to='#' className={css.Pembatas}>
                 |
               </p>
-              <Link to="/profile" className={css.list}>
+              <Link to='/profile' className={css.list}>
                 Profile
               </Link>
-              <p to="#" className={css.Pembatas}>
+              <p to='#' className={css.Pembatas}>
                 |
               </p>
-              <Link to="/lapangan" className={css.list}>
+              <Link to='/lapangan' className={css.list}>
                 Booking Fields
               </Link>
-              <p to="#" className={css.Pembatas}>
+              <p to='#' className={css.Pembatas}>
                 |
               </p>
-              <Link to="/lapangan" className={css.list}>
+              <Link to='/lapangan' className={css.list}>
                 Payment Field Fields
               </Link>
-              <p to="#" className={css.Pembatas}>
+              <p to='#' className={css.Pembatas}>
                 |
               </p>
-              <Link to="/historypayment" className={css.list}>
+              <Link to='/historypayment' className={css.list}>
                 History Payment
               </Link>
             </>
-          ) : profile.role == "owner" ? (
+          ) : profile.role == 'owner' ? (
             <>
-              <Link to="/dashboard" className={css.list}>
+              <Link to='/dashboard' className={css.list}>
                 Dashboard
               </Link>
-              <p to="#" className={css.Pembatas}>
+              <p to='#' className={css.Pembatas}>
                 |
               </p>
-              <Link to="/profile" className={css.list}>
+              <Link to='/profile' className={css.list}>
                 Profile
               </Link>
-              <p to="#" className={css.Pembatas}>
+              <p to='#' className={css.Pembatas}>
                 |
               </p>
-              <Link to="/fields" className={css.list}>
+              <Link to='/fields' className={css.list}>
                 Fields
               </Link>
-              <p to="#" className={css.Pembatas}>
+              <p to='#' className={css.Pembatas}>
                 |
               </p>
-              <Link to="/paymentowner" className={css.list}>
+              <Link to='/paymentowner' className={css.list}>
                 Payment
               </Link>
-              <p to="#" className={css.Pembatas}>
+              <p to='#' className={css.Pembatas}>
                 |
               </p>
-              <Link to="#" className={css.list}>
+              <Link to='#' className={css.list}>
                 History Payment
               </Link>
             </>
           ) : profile.role == 'admin' ? (
             <>
-              <Link to="/dashboard" className={css.list}>
+              <Link to='/dashboard' className={css.list}>
                 Dashboard
               </Link>
-              <p to="#" className={css.Pembatas}>
+              <p to='#' className={css.Pembatas}>
                 |
               </p>
-              <Link to="#" className={css.list}>
+              <Link to='#' className={css.list}>
                 Suspend Account
               </Link>
-              <p to="#" className={css.Pembatas}>
+              <p to='#' className={css.Pembatas}>
                 |
               </p>
-              <Link to="#" className={css.list}>
+              <Link to='#' className={css.list}>
                 Management
               </Link>
             </>
           ) : null}
           {token != null ? (
             <>
-              <p to="#" className={css.Pembatas}>
+              <p to='#' className={css.Pembatas}>
                 |
               </p>
-              <Button type="primary" danger>Logout</Button>
+              <Button
+                type='primary'
+                danger
+                onClick={() => setIsModalOpen(true)}
+              >
+                Logout
+              </Button>
             </>
           ) : (
             <>
-              <p to="#" className={css.Pembatas}>
+              <p to='#' className={css.Pembatas}>
                 |
               </p>
-              <Link to="/" className={css.list}>
+              <Link to='/' className={css.list}>
                 Login
               </Link>
-              <p to="#" className={css.Pembatas}>
+              <p to='#' className={css.Pembatas}>
                 |
               </p>
             </>
           )}
         </div>
+        <Modal
+          width={400}
+          title='Apakah anda yakin ingin keluar?'
+          open={isModalOpen}
+          onCancel={() => setIsModalOpen(false)}
+          onOk={handleLogout}
+          footer={[
+            <Button type='primary' danger onClick={handleLogout}>
+              Logout
+            </Button>,
+            <Button type='primary' onClick={() => setIsModalOpen(false)}>
+              Cancel
+            </Button>,
+          ]}
+        />
       </div>
     </>
   );
